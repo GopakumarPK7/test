27760fc14fe57d782d8872fa1ade6fa762e714bb
GopakumarPK7 <gopakumarpk7@gmail.com>
2022-07-25 12:55:41 +0530

    commit1

---
+++ src/test/java/org/mavensample/project/PracticeMaven.java
@@ -0,0 +1,24 @@
+package org.mavensample.project;
+
+import org.testng.annotations.Test;
+
+public class PracticeMaven 
+{
+@Test
+public void practiceMaven()
+{
+System.out.println("TC -");
+}
+@Test
+public void practiceMaven1()
+{
+System.out.println("TC - 2");
+
+}
+@Test
+public void practiceMaven2()
+{
+System.out.println("TC - 3");
+System.out.println("TC - 5");
+}
+}
